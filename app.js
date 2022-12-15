5d84927002d826ccc2b6c7cde4102ff4c1ec2716
Your name <emilien.lentes@gmail.com>
2022-12-15 11:46:48 +0100

    commit back US5-envent

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-require('dotenv').config();;
-require('./models/connection');
+require("dotenv").config();
+require("./models/connection");
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var eventsRouter = require('./routes/events');
-var artistsRouter = require('./routes/artists');
+var indexRouter = require("./routes/index");
+var eventsRouter = require("./routes/events");
+var artistsRouter = require("./routes/artists");
 
 var app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/events', eventsRouter);
-app.use('/artists', artistsRouter);
+app.use("/", indexRouter);
+app.use("/events", eventsRouter);
+app.use("/", eventsRouter);
+app.use("/artists", artistsRouter);
 
 module.exports = app;
--- routes/events.js
+++ routes/events.js
@@ -1,18 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-require('../models/connection');
-const Event = require('../models/events');
+require("../models/connection");
+const Event = require("../models/events");
 
 // add events
-router.post('/add', function(req, res) {
+router.post("/add", function (req, res) {
   const newEvent = new Event({
     name: req.body.name,
     image: req.body.image,
-  })
-  newEvent.save()
-  .then(
-    res.json({result: true, message: 'event add'})
-  )
+  });
+  newEvent.save().then(res.json({ result: true, message: "event add" }));
 });
+
+router.get("/", (req, res) => {
+  Event.find().then((data) => {
+    res.json({ allEvent: data });
+  });
+});
+
+
+
 
 module.exports = router;
